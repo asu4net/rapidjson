a15e741e4110dc937bc3dd34a00823701e827d18
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2023-04-29 15:33:40 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "rapidjson"
+    kind "StaticLib"
+    language "C++"
+	  staticruntime "off"
+    
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h"
+	}
+
+	filter "system:windows"
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+        defines "NIT_DEBUG"
+        runtime "Debug"
+        symbols "on"
+    
+    filter "configurations:Release"
+        defines "NIT_RELEASE"
+        runtime "Release"
+        optimize "on"
